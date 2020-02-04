16419036cb1d2308dc8c85f7f5484e667d4415ae
ivan <ivanramo148248s@gmail.com>
2020-02-03 19:58:00 -0800

    add png photos

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -4,11 +4,19 @@ import './Styles/Body.scss';
 var Body = () => {
   return(
     <div className="content">
-      <div class="grid-container">
-        <div class="item1">1</div>
-        <div class="item2">2</div>
-        <div class="item3">3</div>
-        <div class="item4">4</div>
+      <div className="grid-container">
+        <div className="item1">
+          <span className="img1" ></span>
+        </div>
+        <div className="item2">
+          
+        </div>
+        <div className="item3">
+
+        </div>
+        <div className="item4">
+
+        </div>
       </div>
     </div>
   )
